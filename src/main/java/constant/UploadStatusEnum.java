9827606128f4456a069b1cb6b2f76919dea97fc3
jinzhimin <jinzhiminCN@163.com>
2018-11-08 21:10:18 +0800

    增加logback.xml 和 UploadStatusEnum

---
+++ src/main/java/constant/UploadStatusEnum.java
@@ -0,0 +1,32 @@
+package constant;
+
+/**
+ * @author jinzhimin
+ * @description: FTP文件上传状态的枚举类
+*/
+public enum UploadStatusEnum {
+    /**
+     * 断点上传成功。
+     */
+    Upload_From_Break_Success,
+    /**
+     * 断点上传失败。
+     */
+    Upload_From_Break_Failed,
+    /**
+     * 新文件上传成功。
+     */
+    Upload_New_File_Success,
+    /**
+     * 新文件上传失败。
+     */
+    Upload_New_File_Failed,
+    /**
+     * 文件已经存在。
+     */
+    File_Exits,
+    /**
+     * 远程文件比本地文件大。
+     */
+    Remote_Bigger_Local,
+}
