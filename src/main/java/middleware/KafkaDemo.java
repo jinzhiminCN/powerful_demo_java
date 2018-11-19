f446672887a276ba99b5f52728ebb24f3dd348a3
jinzhimin <jinzhiminCN@163.com>
2018-11-19 20:37:35 +0800

    add KafkaDemo

---
+++ src/main/java/middleware/KafkaDemo.java
@@ -0,0 +1,114 @@
+package middleware;
+
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.Cluster;
+import org.apache.kafka.common.PartitionInfo;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.*;
+
+/**
+ * @author jinzhimin
+ * @description: kafka测试。
+ */
+public class KafkaDemo {
+    private static final Logger logger = LoggerFactory.getLogger(KafkaDemo.class);
+
+    public static void main(String[] args) {
+        produce();
+//        consume();
+    }
+
+    public static void consume(){
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG ,"test") ;
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+        Consumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList("page_visits"));
+
+        Duration duration = Duration.ofMillis(100);
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(duration);
+            for (ConsumerRecord<String, String> record : records) {
+                logger.info("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
+            }
+        }
+    }
+
+    public static void produce() {
+        long events = 10000L;
+        Random rnd = new Random();
+
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        props.put(ProducerConfig.ACKS_CONFIG, "all");
+        props.put(ProducerConfig.RETRIES_CONFIG, 0);
+        props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
+        props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
+        props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        // 配置partitionner选择策略，可选配置
+        // props.put("partitioner.class", "");
+
+        Producer<String, String> producer = new KafkaProducer<>(props);
+
+        for (long nEvents = 0; nEvents < events; nEvents++) {
+            long runtime = System.currentTimeMillis();
+            String ip = "192.168.2." + rnd.nextInt(255);
+            String msg = runtime + ",www.example.com," + ip;
+            ProducerRecord<String, String> data = new ProducerRecord<String, String>("page_visits", ip, msg);
+            producer.send(data,
+                    new Callback() {
+                        @Override
+                        public void onCompletion(RecordMetadata metadata, Exception e) {
+                            if(e != null) {
+                                e.printStackTrace();
+                            } else {
+                                logger.info("The offset of the record we just sent is: " + metadata.offset());
+                            }
+                        }
+                    });
+        }
+        producer.close();
+    }
+
+}
+
+class SimplePartitioner implements Partitioner {
+
+    @Override
+    public void configure(Map<String, ?> configs) {
+
+    }
+
+    @Override
+    public int partition(String topic, Object key, byte[] keyBytes,
+                         Object value, byte[] valueBytes, Cluster cluster) {
+        int partition = 0;
+        List<PartitionInfo> partitions = cluster.partitionsForTopic(topic);
+        int numPartitions = partitions.size();
+        String stringKey = (String) key;
+        int offset = stringKey.lastIndexOf('.');
+        if (offset > 0) {
+            partition = Integer.parseInt( stringKey.substring(offset+1)) % numPartitions;
+        }
+
+        return partition;
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+}
