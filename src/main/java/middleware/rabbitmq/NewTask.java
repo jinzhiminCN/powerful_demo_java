b3c7939d20c77df423c42a182c32b4318740c101
jinzhimin <jinzhiminCN@163.com>
2019-05-06 10:10:50 +0800

    add basic rabbitmq test from tutorials

---
+++ src/main/java/middleware/rabbitmq/NewTask.java
@@ -0,0 +1,33 @@
+package middleware.rabbitmq;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
+
+/**
+ * @author jinzhimin
+ * @description:
+ * https://github.com/rabbitmq/rabbitmq-tutorials/blob/master/java/NewTask.java
+ */
+public class NewTask {
+    private static final String TASK_QUEUE_NAME = "task_queue";
+
+    public static void main(String[] argv) throws Exception {
+        argv = new String[]{"First", "Second", "Third", "Fourth"};
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel()) {
+            channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+
+            String message = String.join(" ", argv);
+
+            channel.basicPublish("", TASK_QUEUE_NAME,
+                MessageProperties.PERSISTENT_TEXT_PLAIN,
+                message.getBytes("UTF-8"));
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+    }
+}
