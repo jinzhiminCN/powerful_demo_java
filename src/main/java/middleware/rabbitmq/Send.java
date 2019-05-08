45065b847bd45e70aea5cff45e6844ef35eb83a7
jinzhimin <jinzhiminCN@163.com>
2019-05-08 13:50:37 +0800

    add EmitLogTopic and ReceiverLogsTopic

--- src/main/java/middleware/rabbitmq/Send.java
+++ src/main/java/middleware/rabbitmq/Send.java
@@ -5,21 +5,22 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 /**
  * @author jinzhimin
- * @description:
- * https://github.com/rabbitmq/rabbitmq-tutorials/blob/master/java/Send.java
+ * @description: https://github.com/rabbitmq/rabbitmq-tutorials/blob/master/java/Send.java
  */
 public class Send {
-    private final static String QUEUE_NAME = "hello";
+  private static final String QUEUE_NAME = "hello";
 
-    public static void main(String[] argv) throws Exception {
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("localhost");
-        try (Connection connection = factory.newConnection();
-            Channel channel = connection.createChannel()) {
-            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-            String message = "Hello World!";
-            channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
-            System.out.println(" [x] Sent '" + message + "'");
-        }
+  public static void main(String[] argv) throws Exception {
+    ConnectionFactory factory = new ConnectionFactory();
+    factory.setHost("localhost");
+    try (Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel()) {
+      channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+      for (int i = 0; i < 5; i++) {
+        String message = "Hello World!";
+        channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+        System.out.println(" [x] Sent '" + message + "'");
+      }
     }
+  }
 }
